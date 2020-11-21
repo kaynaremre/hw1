94e65c92a5cac99fb4d97f4e2b614886ae1b9663
kaynaremre <emrekaynar_8@hotmail.com>
2020-11-22 02:49:55 +0300

    second

--- homework1_step1.c
+++ homework1_step1.c
@@ -15,7 +15,7 @@ void entrance()
     
     doNotTouch[4] = (long long)secret + 15;
     void* osas = secret;
-    doNotTouch[5] = (long long)osas;
+    doNotTouch[6] = (long long)osas;
 
     // can only modify this section END
     printf("now inside entrance()!\n");
